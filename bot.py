aae2d567d45f5053a04f7fd8ac5943e168e0af37
maxresults4u-png <maxresults4u@gmail.com>
2026-03-03 10:32:41 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,3 +11,5 @@ app = Flask(__name__)
 
 API_KEY_ID = os.getenv("COINBASE_API_KEY_ID")
 PRIVATE_KEY_BASE64 = os.getenv("COINBASE_PRIVATE_KEY")
+print("API_KEY_ID:", API_KEY_ID)
+print("PRIVATE_KEY exists:", PRIVATE_KEY_BASE64 is not None)
